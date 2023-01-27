84aff5461ef36e8a6ed3d96f00792c256878ad92
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-01-26 21:09:51 -0500

    post lecture code complete

--- 01_ruby_fundamentals/lib/dog_walker_cli.rb
+++ 01_ruby_fundamentals/lib/dog_walker_cli.rb
@@ -2,6 +2,8 @@ def start_cli
   puts "Hi there! Welcome to the Dog Walker CLI!".cyan
 end
 
+
+
 # define a method `print_menu_options` which outlines the user's
 # choices. The choices will be displayed as a numbered list like so:
 #   1. List all dogs
@@ -9,14 +11,27 @@ end
 # At any time, type "menu" to see these options again or "exit" to
 # leave the program
 
+def print_menu_options
+  puts "  1. List all dogs"
+  puts "  2. Add a dog"
+  puts "  At any time, type 'menu' to see these options again or 'exit' to leave the program"
+end
 
 
 
 # define a method `ask_for_choice` which prompts the user for input
 # if the user types "exit" we'll print a message thanking them
-# for using the CLI and invoke exit to terminate the program
+# for using the CLI and invoke to terminate the program
 # otherwise, return whatever the user typed in
-
+def ask_for_choice
+  input = gets.chomp
+  # if input is exit, we want to exit. don't want input = "exit"
+  if input == "exit"
+    puts "That was fast. Thanks for playing, please come back soon."
+    exit
+  end
+  input
+end
 
 
 # define a `print_dog` method that accepts a dog hash as a parameter
@@ -29,13 +44,24 @@ Lennon Snow
   Image URL: https://res.cloudinary.com/dnocv6uwb/image/upload/v1609370267/dakota-and-lennon-square-compressed_hoenfo.jpg
 
 =end
-
+def print_dog(dog_hash)
+  puts
+  puts dog_hash[:name].blue
+  puts "  Age: #{dog_hash[:age]}"
+  puts "  Breed: #{dog_hash[:breed]}"
+  puts "  Image URL: #{dog_hash[:image_url]}"
+  puts
+end
 
 
 
 # define a method `list_dogs` that will iterate over an array of
 # dog hashes and call print_dog on each one.
-
+def list_dogs(dog_arr)
+  dog_arr.each do |dog_hash|
+  print_dog(dog_hash)
+  end
+end
 
 
 # define an `add_dog` method which accepts an array of dog
